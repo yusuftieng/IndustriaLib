24ac7952a4c8aab7802c16ffb4e60e136e69cbbd
Abdulkadir Yusuf Tunc <abdulkadiryusuftunc@gmail.com>
2026-03-23 21:27:59 +0300

    Update vite.config.ts

--- industrialib (13)/vite.config.ts
+++ industrialib (13)/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/IndustriaLib/',
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
